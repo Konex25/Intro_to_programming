5966aa8d6806327ed5c4cf6d1827c898bd33b6d1
Konex25 <rapacz.konrad@gmail.com>
2025-12-02 23:01:54 +0100

    Task2H

---
+++ src/Task2H.java
@@ -0,0 +1,5 @@
+// ArrayList is slower for inserts and deletes at the beginning,
+// because it must shift many elements in memory.
+
+// LinkedList is slower for random access,
+// because it must walk through nodes one by one.
